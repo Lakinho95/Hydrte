cb41f42bca86da57a1c1cc62d86c4e649f197a71
Lazar Kanazir <lazarkanair995@gmail.com>
2022-11-16 14:33:34 +0100

    changes1

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,9 @@ export default function Home() {
   
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </Head>
       <Okvir>
         <Navbar />
         <Glavno>
