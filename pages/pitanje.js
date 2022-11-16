7ec20acaee4013c14c70adb3ac6bfe5fc8f47da8
Lazar Kanazir <lazarkanair995@gmail.com>
2022-11-16 14:37:10 +0100

    changes2

--- pages/pitanje.js
+++ pages/pitanje.js
@@ -5,9 +5,6 @@ import Forma1 from '../components/kupi/Forma1'
 function Pitanje() {
     return (
         <div>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-            </Head>
             <Navbar1 />
             <Forma1 />
         </div>
